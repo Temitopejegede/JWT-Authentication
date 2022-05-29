e5300d43504e108d554b35401ba96b800a8ee2ee
Temitope Jegede <temiteejegede@gmail.com>
2022-05-29 01:09:24 -0400

    more refractoring

--- errors/index.js
+++ errors/index.js
@@ -2,4 +2,4 @@ const CustomAPIError = require("./custom-error");
 const badRequestError = require("./badRequest");
 const unAuthenticatedError = require("./unAuthenticated");
 
-module.exports = { customAPIError, badRequestError, unAuthenticatedError };
+module.exports = { CustomAPIError, badRequestError, unAuthenticatedError };
